68704af795486457e58eeb598558c1fb56fcf152
DenisTmenov <denis.tmenov@gmail.com>
2017-07-02 17:56:04 +0300

    create new Exception EntityException

--- ProjectM/src/main/java/com/project/m/entity/EntityCreator.java
+++ ProjectM/src/main/java/com/project/m/entity/EntityCreator.java
@@ -4,13 +4,24 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Date;
 
+import com.project.m.exceptions.EntityException;
+import com.project.m.exceptions.SqlException;
+
 public class EntityCreator {
-	
-	public static DboBatchesEntity createDboBatchesEntity(ResultSet set) throws SQLException {
-		Integer batchId = set.getInt("BatchId");
-		String batchName = set.getString("BatchName");
-		Date createdDate = set.getDate("CreatedDate");
-		Boolean isActive = set.getBoolean("IsActive");
+
+	public static DboBatchesEntity createDboBatchesEntity(ResultSet set) {
+		Integer batchId = null;
+		String batchName = null;
+		Date createdDate = null;
+		Boolean isActive = null;
+		try {
+			batchId = set.getInt("BatchId");
+			batchName = set.getString("BatchName");
+			createdDate = set.getDate("CreatedDate");
+			isActive = set.getBoolean("IsActive");
+		} catch (SQLException e) {
+			throw new EntityException("Exception in createDboBatchesEntity().", e);
+		}
 
 		DboBatchesEntity entity = new DboBatchesEntity();
 
@@ -21,7 +32,5 @@ public class EntityCreator {
 
 		return entity;
 	}
-	
-	
 
 }
