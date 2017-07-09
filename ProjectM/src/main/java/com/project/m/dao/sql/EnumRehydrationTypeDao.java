e8d5ae1022b29d566a2ea6243c55e3bc21c0db57
DenisTmenov <denis.tmenov@gmail.com>
2017-07-09 15:57:48 +0300

    added new entity(enum from DB)

---
+++ ProjectM/src/main/java/com/project/m/dao/sql/EnumRehydrationTypeDao.java
@@ -0,0 +1,55 @@
+package com.project.m.dao.sql;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.project.m.dao.EnumRehydrationTypeDaoInterface;
+import com.project.m.dao.db.ConnectionPool;
+import com.project.m.entity.EntityBatches;
+import com.project.m.exceptions.DaoException;
+
+public class EnumRehydrationTypeDao implements EnumRehydrationTypeDaoInterface {
+
+	@Override
+	public void save(EntityBatches bean) throws DaoException {
+	}
+
+	@Override
+	public void update(EntityBatches bean) throws DaoException {
+	}
+
+	@Override
+	public void remove(Integer batchId) throws DaoException {
+	}
+
+	@Override
+	public Map<Integer, String> loadEnumRehydrationType() {
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet set = null;
+
+		Map<Integer, String> result = new HashMap<>();
+
+		try {
+			connection = ConnectionPool.getInstance().getConnection();
+			statement = connection.prepareStatement("SELECT * FROM [dbo].[Enum_RehydrationType]");
+
+			set = statement.executeQuery();
+
+			while (set.next()) {
+				result.put(set.getInt("Id"), set.getString("Description"));
+			}
+		} catch (SQLException e) {
+			throw new DaoException("Exception in loadEnumMigrationType().", e);
+		} finally {
+			ConnectionPool.closeDbResources(connection, statement, set);
+		}
+
+		return result;
+	}
+
+}
