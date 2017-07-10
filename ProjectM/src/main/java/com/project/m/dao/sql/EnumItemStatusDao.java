ef4500fe2062cb6a147f7935c568e3521e49d71a
DenisTmenov <denis.tmenov@gmail.com>
2017-07-10 17:21:04 +0300

    added new Dao

---
+++ ProjectM/src/main/java/com/project/m/dao/sql/EnumItemStatusDao.java
@@ -0,0 +1,42 @@
+package com.project.m.dao.sql;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.project.m.dao.EnumItemStatusDaoInterface;
+import com.project.m.dao.db.ConnectionPool;
+import com.project.m.exceptions.DaoException;
+
+public class EnumItemStatusDao implements EnumItemStatusDaoInterface {
+
+	@Override
+	public Map<Integer, String> loadEnumItemStatus() {
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet set = null;
+
+		Map<Integer, String> result = new HashMap<>();
+
+		try {
+			connection = ConnectionPool.getInstance().getConnection();
+			statement = connection.prepareStatement("SELECT * FROM [dbo].[Enum_JobStatus]");
+
+			set = statement.executeQuery();
+
+			while (set.next()) {
+				result.put(set.getInt("Id"), set.getString("Description"));
+			}
+		} catch (SQLException e) {
+			throw new DaoException("Exception in loadJobHistoriesByBatchId().", e);
+		} finally {
+			ConnectionPool.closeDbResources(connection, statement, set);
+		}
+
+		return result;
+	}
+
+}
